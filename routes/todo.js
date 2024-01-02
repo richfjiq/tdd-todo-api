3c6b90a1bd4e156f0f233f996105c943e6dafcf2
richfjiq <rfjiq1986@gmail.com>
2024-01-02 16:32:40 -0600

    chore: adds tests to HTTP DELETE route

--- routes/todo.js
+++ routes/todo.js
@@ -7,5 +7,6 @@ router.get('/:id', todoController.getTodoById);
 router.get('/', todoController.getTodos);
 router.post('/', todoController.createTodo);
 router.put('/:id', todoController.updateTodo);
+router.delete('/:id', todoController.deleteTodo);
 
 module.exports = router;
